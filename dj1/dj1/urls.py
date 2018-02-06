eceafb423c3f853876eae8958ec03db4e4bd886c
speakfly <1090384046@qq.com>
2018-02-06 10:03:46 +0800

    test

--- dj1/dj1/urls.py
+++ dj1/dj1/urls.py
@@ -15,7 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from app import views as app_views
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path(r'test/$', app_views.test),
 ]
