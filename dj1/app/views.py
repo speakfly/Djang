5c7af1d1edf6b2561cb5ed6384cf5591812dd30b
speakfly <1090384046@qq.com>
2018-02-06 12:43:31 +0800

    test

--- dj1/app/views.py
+++ dj1/app/views.py
@@ -3,8 +3,8 @@ from django.http import HttpResponseRedirect,HttpResponse
 from django import forms
 # Create your views here.
 class UserForm(forms.Form):
-	account  = forms.CharField(max_length = 20)
-	password = forms.CharField(max_length = 20)
+    account  = forms.CharField(max_length = 20)
+    password = forms.CharField(max_length = 20)
 
 class PasswordForm(forms.Form):
 	old_password  = forms.CharField(max_length = 20)
@@ -25,10 +25,10 @@ def login(request):
 	return render(request,'login.html',{'uf':'uf'})
 
 def index(request):
-	account = request.session.get("account","anybody")
-	users = User.objects.filter(account=account)
-	if len(users) == 0:
-		return HttpResponseRedirect('/login/')
+    account = request.session.get("account","anybody")
+    users = User.objects.filter(account=account)
+    if len(users) == 0:
+        return HttpResponseRedirect('/login/')
     led_statu = users[0].led_statu
     return render(request,'index.html',{'led_statu':led_statu})
 
@@ -41,24 +41,24 @@ def logout(request):
 	return HttpResponseRedirect("/login/")
 
 def updatepassword(request):
-	account = request.session.get("account","anybody")
-	users = User.objects.filter(account=account)
-	if len(users) == 0:
-		return HttpResponseRedirect('/login/')
-	if request.method == 'POST':
-		uf = PasswordForm(request.POST)
-		if uf.is_valid():
-			old_password = uf.cleaned_data['old_password']
-			new_password1 = uf.cleaned_data['new_password1']
-			new_password2 = uf.cleaned_data['new_password2']
-			if new_password1 != new_password2:
-				return HttpResponse(u"两次密码不匹配")
-			users = User.objects.filter(account=account,password=old_password)
-			if len(users)==0:
-				return HttpResponse(u"旧密码错误")
-			Student.objects.filter(account=account).update(password=new_password2)
-			return HttpResponseRedirect('/index/')
-	return render(request,'updatepassword.html')
+    account = request.session.get("account","anybody")
+    users = User.objects.filter(account=account)
+    if len(users) == 0:
+        return HttpResponseRedirect('/login/')
+    if request.method == 'POST':
+        uf = PasswordForm(request.POST)
+        if uf.is_valid():
+            old_password = uf.cleaned_data['old_password']
+            new_password1 = uf.cleaned_data['new_password1']
+            new_password2 = uf.cleaned_data['new_password2']
+            if new_password1 != new_password2:
+                return HttpResponse(u"两次密码不匹配")
+            users = User.objects.filter(account=account,password=old_password)
+            if len(users)==0:
+                return HttpResponse(u"旧密码错误")
+            User.objects.filter(account=account).update(password=new_password2)
+            return HttpResponseRedirect('/index/')
+    return render(request,'updatepassword.html')
 
 def close_led(request):
     account = request.session.get("account","anybody")
